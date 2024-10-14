3e760d796221b6e84e2fd274c35ef74c27e46a76
TuTuanLyan <nguyentuanhtuan313@gmail.com>
2024-10-14 21:03:13 +0700

    Update login and register form, delete no longer in use file

--- src/main/java/com/lightlibrary/Controllers/LoginAndRegisterController.java
+++ src/main/java/com/lightlibrary/Controllers/LoginAndRegisterController.java
@@ -22,7 +22,7 @@ import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class LoginAndRegister implements Initializable {
+public class LoginAndRegisterController implements Initializable {
 
     @FXML
     private AnchorPane sliderContainer;
