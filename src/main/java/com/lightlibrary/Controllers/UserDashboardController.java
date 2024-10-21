0faaffcddf75f0ab4339411512ea8fa08c193107
TuTuanLyan <nguyentuanhtuan313@gmail.com>
2024-10-21 08:58:25 +0700

    Set animation for navigation in user dashboard

--- src/main/java/com/lightlibrary/Controllers/UserDashboardController.java
+++ src/main/java/com/lightlibrary/Controllers/UserDashboardController.java
@@ -3,16 +3,22 @@ package com.lightlibrary.Controllers;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class UserDashboardController implements Initializable {
@@ -27,12 +33,40 @@ public class UserDashboardController implements Initializable {
     private AnchorPane individualButtonContainer;
     private static boolean individualBarShown = false;
 
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private AnchorPane navigationButtonBorder;
+
+    @FXML
+    private Button issueBookButton;
+
+    @FXML
+    private Button returnBookButton;
+
+    @FXML
+    private Button supportButton;
+
+    enum ActiveButton {
+        DASHBOARD,
+        ISSUE_BOOK,
+        RETURN_BOOK,
+        SUPPORT
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         individualButtonContainer.setVisible(false);
         Circle avatarClip = new Circle(28, 28, 28);
         avatarContainer.setClip(avatarClip);
-        //avatarImage.setClip(avatarClip);
+
+        dashboardButton.getStyleClass().add("selected");
+
+        dashboardButton.setOnAction(e -> handleNavigationButtonBorder(dashboardButton));
+        issueBookButton.setOnAction(e -> handleNavigationButtonBorder(issueBookButton));
+        returnBookButton.setOnAction(e -> handleNavigationButtonBorder(returnBookButton));
+        supportButton.setOnAction(e -> handleNavigationButtonBorder(supportButton));
     }
 
     @FXML
@@ -56,4 +90,26 @@ public class UserDashboardController implements Initializable {
         individualBarTransition.play();
     }
 
+    private void handleNavigationButtonBorder(Button activeButton) {
+        TranslateTransition navigationBarTransition = new TranslateTransition();
+        navigationBarTransition.setNode(navigationButtonBorder);
+        navigationBarTransition.setDuration(Duration.seconds(0.2));
+        navigationBarTransition.setToY(activeButton.getLayoutY() - navigationButtonBorder.getLayoutY());
+        navigationBarTransition.play();
+
+        dashboardButton.getStyleClass().remove("selected");
+        issueBookButton.getStyleClass().remove("selected");
+        returnBookButton.getStyleClass().remove("selected");
+        supportButton.getStyleClass().remove("selected");
+
+        activeButton.getStyleClass().add("selected");
+    }
+
+    @FXML
+    protected void handleLogoutAction(ActionEvent event) throws IOException {
+        Parent login = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/lightlibrary/Views/LoginAndRegister.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(login, 960, 640));
+        stage.show();
+    }
 }
