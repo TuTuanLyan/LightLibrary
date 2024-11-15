fb11e92fcccd2d21d9a46507bc06b3758cdb800a
Bống Bống <pieces.nyan@gmail.com>
2024-11-16 01:27:13 +0700

    fix titleGrid and create search user in sql

--- src/main/java/com/lightlibrary/Controllers/AdminUserManagementController.java
+++ src/main/java/com/lightlibrary/Controllers/AdminUserManagementController.java
@@ -3,11 +3,13 @@ package com.lightlibrary.Controllers;
 import com.lightlibrary.Models.DatabaseConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 
 import java.net.URL;
 import java.sql.Connection;
@@ -16,6 +18,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AdminUserManagementController implements Initializable, SyncAction {
@@ -24,11 +28,31 @@ public class AdminUserManagementController implements Initializable, SyncAction 
     private AnchorPane userManagementRoot;
 
     @FXML
-    private GridPane gridPane;
+    private GridPane manageUserGrid;
+
+    @FXML
+    private GridPane titleGrid;
 
     @FXML
     private AnchorPane editPane;
 
+    @FXML
+    private TextField sortEmail;
+
+    @FXML
+    private TextField sortFullname;
+
+    @FXML
+    private TextField sortPassword;
+
+    @FXML
+    private TextField sortPhone;
+
+    @FXML
+    private TextField sortUserID;
+
+    @FXML
+    private TextField sortUsername;
 
     AdminDashboardController parentController;
 
@@ -44,6 +68,24 @@ public class AdminUserManagementController implements Initializable, SyncAction 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         reLoad();
+        sortUserID.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
+        sortFullname.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
+        sortPassword.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
+        sortUsername.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
+        sortEmail.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
+        sortPhone.setOnKeyTyped(keyEvent -> {
+            reLoad();
+        });
     }
 
     @Override
@@ -63,41 +105,174 @@ public class AdminUserManagementController implements Initializable, SyncAction 
 
 
     public void reLoad() {
-        while (gridPane.getChildren().size() >= 8) {
-            gridPane.getChildren().remove(gridPane.getChildren().size() - 1);
+        while (manageUserGrid.getChildren().size() > 1) {
+            manageUserGrid.getChildren().removeLast();
         }
+        String findFullname = sortFullname.getText();
+        String findUsername = sortUsername.getText();
+        String findPassword = sortPassword.getText();
+        String findPhone = sortPhone.getText();
+        String findEmail = sortEmail.getText();
         Connection connectDB = DatabaseConnection.getConnection();
 
         if (connectDB == null) {
             System.out.println("Something were wrong connectDB is null!");
         }
 
-        String connectQuery = "SELECT * FROM users";
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        if(!sortPhone.getText().isEmpty()) {
+            if(!sortUserID.getText().isEmpty()) {
+                int findUserID = -1;
+                try {
+                    findUserID = Integer.parseInt(sortUserID.getText());
+                } catch (NumberFormatException e) {
+                    System.out.println("wrong userID");
+                }
+                String connectQuery = "SELECT * FROM users WHERE userID like ? and fullName LIKE ? and username LIKE ? and password LIKE ? and phoneNumber LIKE ? and email LIKE ?";
 
-        try {
-            PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                int userID = resultSet.getInt("userID");
-                String fullname = resultSet.getString("fullname");
-                String username = resultSet.getString("username");
-                String password = resultSet.getString("password");
-                String phoneNumber = resultSet.getString("phoneNumber");
-                String email = resultSet.getString("email");
-                add(userID, fullname, username, password, phoneNumber, email);
+                try {
+                    PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
+                    preparedStatement.setInt(1, findUserID);
+                    preparedStatement.setString(2, "%" + findFullname + "%");
+                    preparedStatement.setString(3, "%" + findUsername + "%");
+                    preparedStatement.setString(4, "%" + findPassword + "%");
+                    preparedStatement.setString(5, "%" + findPhone + "%");
+                    preparedStatement.setString(6, "%" + findEmail + "%");
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if(resultSet == null) {
+                        System.out.println("find someone with: userID=" + findUserID + " and phone:" + findPhone);
+                    }
+                    else System.out.println("find no one with: userID=" + findUserID + " and phone:" + findPhone);
+                    while (resultSet.next()) {
+                        int userID = resultSet.getInt("userID");
+                        String fullname = resultSet.getString("fullName");
+                        String username = resultSet.getString("username");
+                        String password = resultSet.getString("password");
+                        String phoneNumber = resultSet.getString("phoneNumber");
+                        String email = resultSet.getString("email");
+                        System.out.println(userID + " " + fullname + " " + username + " " + password + " " + phoneNumber + " " + email);
+                        add(userID, fullname, username, password, phoneNumber, email);
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
+            else {
+                String connectQuery = "SELECT * FROM users WHERE fullName LIKE ? and username LIKE ? and password LIKE ? and phoneNumber LIKE ? and email LIKE ?;";
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+                try {
+                    PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
+                    preparedStatement.setString(1, "%" + findFullname + "%");
+                    preparedStatement.setString(2, "%" + findUsername + "%");
+                    preparedStatement.setString(3, "%" + findPassword + "%");
+                    preparedStatement.setString(4, "%" + findPhone + "%");
+                    preparedStatement.setString(5, "%" + findEmail + "%");
 
+                    System.out.println(preparedStatement);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if(resultSet == null) {
+                        System.out.println("find someone with phone: " + findPhone);
+                    }
+                    else System.out.println("find no one with phone: " + findPhone);
+                    while (resultSet.next()) {
+                        int userID = resultSet.getInt("userID");
+                        String fullname = resultSet.getString("fullName");
+                        String username = resultSet.getString("username");
+                        String password = resultSet.getString("password");
+                        String phoneNumber = resultSet.getString("phoneNumber");
+                        String email = resultSet.getString("email");
+                        System.out.println(userID + " " + fullname + " " + username + " " + password + " " + phoneNumber + " " + email);
+                        add(userID, fullname, username, password, phoneNumber, email);
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         /*for (Node node : gridPane.getChildren()){
             node.setStyle("-fx-border-color: black; -fx-border-width: 1;");
         }*/
+        }
+        else {
+            if(!sortUserID.getText().isEmpty()) {
+                int findUserID = 0;
+                try {
+                    findUserID = Integer.parseInt(sortUserID.getText());
+                } catch (NumberFormatException e) {
+                    System.out.println("wrong userID");
+                }
+                String connectQuery = "SELECT * FROM users WHERE userID like ? and fullName LIKE ? and username LIKE ? and password LIKE ? and email LIKE ?";
+
+                try {
+                    PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
+                    preparedStatement.setInt(1, findUserID);
+                    preparedStatement.setString(2, "%" + findFullname + "%");
+                    preparedStatement.setString(3, "%" + findUsername + "%");
+                    preparedStatement.setString(4, "%" + findPassword + "%");
+                    preparedStatement.setString(5, "%" + findEmail + "%");
+
+                    System.out.println(preparedStatement);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if(resultSet == null) {
+                        System.out.println("find someone");
+                    }
+                    else System.out.println("find nothing");
+                    while (resultSet.next()) {
+                        int userID = resultSet.getInt("userID");
+                        String fullname = resultSet.getString("fullName");
+                        String username = resultSet.getString("username");
+                        String password = resultSet.getString("password");
+                        String phoneNumber = resultSet.getString("phoneNumber");
+                        String email = resultSet.getString("email");
+                        System.out.println(userID + " " + fullname + " " + username + " " + password + " " + phoneNumber + " " + email);
+                        add(userID, fullname, username, password, phoneNumber, email);
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                String connectQuery = "SELECT * FROM users WHERE fullName LIKE ? and username LIKE ? and password LIKE ? and email LIKE ?;";
+
+                try {
+                    PreparedStatement preparedStatement = connectDB.prepareStatement(connectQuery);
+                    preparedStatement.setString(1, "%" + findFullname + "%");
+                    preparedStatement.setString(2, "%" + findUsername + "%");
+                    preparedStatement.setString(3, "%" + findPassword + "%");
+                    preparedStatement.setString(4, "%" + findEmail + "%");
+
+                    System.out.println(preparedStatement);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if(resultSet == null) {
+                        System.out.println("find someone");
+                    }
+                    else System.out.println("find nothing");
+                    while (resultSet.next()) {
+                        int userID = resultSet.getInt("userID");
+                        String fullname = resultSet.getString("fullName");
+                        String username = resultSet.getString("username");
+                        String password = resultSet.getString("password");
+                        String phoneNumber = resultSet.getString("phoneNumber");
+                        String email = resultSet.getString("email");
+                        System.out.println(userID + " " + fullname + " " + username + " " + password + " " + phoneNumber + " " + email);
+                        add(userID, fullname, username, password, phoneNumber, email);
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        /*for (Node node : gridPane.getChildren()){
+            node.setStyle("-fx-border-color: black; -fx-border-width: 1;");
+        }*/
+        }
+
     }
 
     public void add(int userID, String fullname, String username, String password, String phoneNumber, String email) {
-        int index = gridPane.getRowCount();
+        int index = manageUserGrid.getRowCount();
         // đống label dùng để nhét vào từng row
         Label userIDLabel = new Label(Integer.toString(userID));
         Label fullnameLabel = new Label(fullname);
@@ -109,7 +284,7 @@ public class AdminUserManagementController implements Initializable, SyncAction 
         Button deleteButton = new Button("Delete");
 
 
-        gridPane.addRow(gridPane.getRowCount(), userIDLabel, fullnameLabel, usernameLabel, passwordLabel, phoneNumberLabel, emailLabel, editButton, deleteButton);
+        manageUserGrid.addRow(manageUserGrid.getRowCount(), userIDLabel, fullnameLabel, usernameLabel, passwordLabel, phoneNumberLabel, emailLabel, editButton, deleteButton);
 
         deleteButton.setOnAction(e -> {
             remove(userID);
@@ -150,7 +325,7 @@ public class AdminUserManagementController implements Initializable, SyncAction 
 
     public void remove(int userID) {
         Connection connectDB = DatabaseConnection.getConnection();
-        String removeQuery = "DELETE FROM users WHERE userID = ?";
+        String removeQuery = "DELETE FROM users WHERE userID = ?;";
         try {
             PreparedStatement preparedStatement = connectDB.prepareStatement(removeQuery);
             preparedStatement.setInt(1, userID);
@@ -166,7 +341,7 @@ public class AdminUserManagementController implements Initializable, SyncAction 
 
     public void edit(int userID, String fullname, String password, String phoneNumber, String email) {
         Connection connectDB = DatabaseConnection.getConnection();
-        String editQuery = "UPDATE users SET fullName = ?,password = ?,phoneNumber = ?,email = ? where userID = ?";
+        String editQuery = "UPDATE users SET fullName = ?,password = ?,phoneNumber = ?,email = ? where userID = ?;";
         try {
             PreparedStatement preparedStatement = connectDB.prepareStatement(editQuery);
             preparedStatement.setString(1, fullname);
