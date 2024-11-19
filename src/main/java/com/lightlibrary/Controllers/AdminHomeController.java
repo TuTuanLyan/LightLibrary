be6d3498bac83cf85f5e8196aacb47c14025f13c
Bống Bống <pieces.nyan@gmail.com>
2024-11-19 21:31:03 +0700

    create viewBook and viewUser in HomepageAdmin

--- src/main/java/com/lightlibrary/Controllers/AdminHomeController.java
+++ src/main/java/com/lightlibrary/Controllers/AdminHomeController.java
@@ -1,5 +1,6 @@
 package com.lightlibrary.Controllers;
 
+import com.lightlibrary.Models.DatabaseConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,8 +10,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.net.URL;
+import java.sql.*;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -62,7 +65,8 @@ public class AdminHomeController implements Initializable, SyncAction {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        loadViewBook();
+        loadViewUser();
     }
 
     @Override
@@ -104,5 +108,67 @@ public class AdminHomeController implements Initializable, SyncAction {
     @FXML
     public void viewAndEditUser(ActionEvent event) {
         parentController.goToUserManagementPage();
+    }
+
+
+    public void loadViewBook() {
+        listBookGridPane.getChildren().clear();
+        listBookGridPane.getRowConstraints().clear();
+
+        Connection connection = DatabaseConnection.getConnection();
+        String queryConnect = "SELECT * FROM books";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queryConnect);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+
+                String ISBN = resultSet.getString("ISBN");
+                String title = resultSet.getString("TITLE");
+                String author = resultSet.getString("AUTHOR");
+                int available = resultSet.getInt("availableNumber");
+                listBookGridPane.addRow(listBookGridPane.getRowCount(),new Label(ISBN),new Label(title),new Label(author),new Label(Integer.toString(available)));
+                RowConstraints curConstraints = new RowConstraints();
+                curConstraints.setMinHeight(70);
+                listBookGridPane.getRowConstraints().add(curConstraints);
+            }
+
+        } catch (SQLException e) {
+                throw new RuntimeException(e);
+        }
+    }
+
+    public void loadViewUser() {
+        listUserGridPane.getChildren().clear();
+        listUserGridPane.getRowConstraints().clear();
+
+        Connection connection = DatabaseConnection.getConnection();
+        String queryConnect = "select " +
+                "u.*," +
+                "case " +
+                "when " +
+                "count(t.userID) > 0 then count(t.userID) " +
+                "else 0 " +
+                "end as totalBorrowed " +
+                "from users u left join transactions t  on u.userID = t.userID " +
+                "group by u.userID " +
+                "order by u.userID;";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(queryConnect);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int userID = resultSet.getInt("userID");
+                String fullName = resultSet.getString("fullName");
+                String userName = resultSet.getString("userName");
+                int totalBorrowed = resultSet.getInt("totalBorrowed");
+
+                listUserGridPane.addRow(listUserGridPane.getRowCount(),new Label(Integer.toString(userID)),new Label(userName),new Label(fullName),new Label(Integer.toString(totalBorrowed)));
+                RowConstraints curConstraints = new RowConstraints();
+                curConstraints.setMinHeight(70);
+                listUserGridPane.getRowConstraints().add(curConstraints);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
