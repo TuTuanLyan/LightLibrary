1e4d0cfeab750d7cc97b3e0e895f2f6e8561b39d
TuTuanLyan <nguyentuanhtuan313@gmail.com>
2024-11-22 20:39:36 +0700

    Design Customer History

--- src/main/java/com/lightlibrary/Controllers/CustomerHistoryController.java
+++ src/main/java/com/lightlibrary/Controllers/CustomerHistoryController.java
@@ -2,7 +2,11 @@ package com.lightlibrary.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.Objects;
@@ -12,6 +16,36 @@ public class CustomerHistoryController implements Initializable, SyncAction {
 
     @FXML
     private AnchorPane historyRoot;
+
+    @FXML
+    private DatePicker borrowDatePicker;
+
+    @FXML
+    private DatePicker dueDatePicker;
+
+    @FXML
+    private TextField fillAuthorField;
+
+    @FXML
+    private TextField fillISBNField;
+
+    @FXML
+    private TextField fillTitleField;
+
+    @FXML
+    private TextField fillTotalField;
+
+    @FXML
+    private GridPane historyTableGrid;
+
+    @FXML
+    private DatePicker returnDatePicker;
+
+    @FXML
+    private Label spenrTotalCoinLabel;
+
+    @FXML
+    private GridPane titleGrid;
 
     CustomerDashboardController parentController;
 
